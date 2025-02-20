e5a1a3425cd1125e71e04f44fb961d8cdf6a706c
hossainsmshakib <smsakib2403@gmail.com>
2025-02-20 12:48:57 +0600

    feat: basic TCP server responding with GoBolt message

--- main.go
+++ main.go
@@ -2,14 +2,16 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net"
+	"os"
 )
 
 func main() {
-	fmt.Print("Listening on the port: 6060")
+	fmt.Print("Listening on the port: 6379")
 
 	// Crete a new server or TCP listener
-	l, err := net.Listen("tcp", ":6060") // l: This stores the TCP listener object (server socket).
+	l, err := net.Listen("tcp", ":6379") // l: This stores the TCP listener object (server socket).
 	if err != nil {
 		fmt.Print(err) // err: Stores any error encountered in net.Listen() or l.Accept().
 		return
@@ -24,4 +26,23 @@ func main() {
 	}
 	defer conn.Close() // Ensures the connection closes properly when the function exits.
 	// Prevents memory leaks by cleaning up resources automatically.
+
+	for { // This creates an infinite loop, meaning the server will continuously read incoming messages from the client.
+		buf := make([]byte, 1024) // Creates a byte slice (buffer) of size 1024 bytes. It will store incoming message from client.
+
+		// read message from client
+		_, err = conn.Read(buf) // The underscore (_) ignores the number of bytes read because it’s not used in the logic.
+		if err != nil {
+			if err == io.EOF { // EOF (End of File): io.EOF: The client closed the connection, so we break the loop.
+				break
+			}
+			fmt.Println("error reading from client: ", err.Error())
+			os.Exit(1)
+		}
+
+		// No matter what message the client sends, the server always responds with +GoBolt says OK\r\n.
+		conn.Write([]byte("+GoBolt says OK\r\n"))
+		// This follows the Redis Simple String format, where a + prefix indicates a plain-text response.
+		//The \r\n (carriage return + newline) is required by the Redis protocol.
+	}
 }
