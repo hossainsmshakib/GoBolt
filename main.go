e2aafcefd9a74a063d91b047b81f2aff0350654b
hossainsmshakib <smsakib2403@gmail.com>
2025-02-20 12:26:48 +0600

    Added server and listener

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+)
 
 func main() {
-    fmt.Println("Hello, World!")
+	fmt.Print("Listening on the port: 6060")
+
+	// Crete a new server or TCP listener
+	l, err := net.Listen("tcp", ":6060") // l: This stores the TCP listener object (server socket).
+	if err != nil {
+		fmt.Print(err) // err: Stores any error encountered in net.Listen() or l.Accept().
+		return
+	}
+
+	// Listening for connection or Receiving request
+	conn, err := l.Accept() // conn: Stores the active client connection (TCP) once a client connects.
+	// l.Accept(): Waits for a client to connect to the server. When a client connects, it returns a connection object (conn).
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer conn.Close() // Ensures the connection closes properly when the function exits.
+	// Prevents memory leaks by cleaning up resources automatically.
 }
